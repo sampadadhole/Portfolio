f385a573fef67adf4739bb00d9abebf3381ea3eb
Shubham Chopade <31898834+shubhamchopade@users.noreply.github.com>
2022-08-01 17:31:42 -0400

    Google analytics and Favicon

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Navbar  from './Navbar';
-import About from './About';
-import Projects from './Projects';
-import Experience from './Experience';
-import LandingPage from './LandingPage';
-import Footer from './Footer';
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import About from "./About";
+import Projects from "./Projects";
+import Experience from "./Experience";
+import LandingPage from "./LandingPage";
+import Footer from "./Footer";
+import ReactGA from "react-ga4";
 
-function Port(){
-    return(
-        <div>
-            <Navbar />
-            <LandingPage />
-            <About />
-            <Experience />
-            <Projects />
-            <Footer />
-            
-        </div>
-    )
+function Port() {
+  return (
+    <div>
+      <Navbar />
+      <LandingPage />
+      <About />
+      <Experience />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 export default Port;
